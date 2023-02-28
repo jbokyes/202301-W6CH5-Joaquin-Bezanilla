f60072365569451ea02daeec71e3be2a981e6120
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-28 18:06:02 +0100

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
+import createDebug from 'debug';
 import http from 'http';
 import { app } from './app.js';
+import { dbConnect } from './db/connect.db.js';
 
-const PORT = process.env.PORT ?? 4500;
+const debug = createDebug('W6');
+
+const PORT = process.env.PORT || 4500;
 
 const server = http.createServer(app);
 
-server.listen(PORT);
-
-server.on('error', () => {
-  console.log('Esto es un error en el server.on, bro');
-}); // Este código es 100% innecesario. Lo dejo para constatar como ejemplo. Profesor mencionó esto durante la clase 1 y se lo repitió a ryan en la clase 2. Lo copié sin saber que hace.
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    debug('DB:', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => server.emit('error', error));
+server.on('error', (error) => {
+  debug('Server error', error.message);
+});
 
 server.on('listening', () => {
-  console.log('Listening in http://localhost:' + PORT);
+  debug('Listening in http://localhost:' + PORT);
 });
